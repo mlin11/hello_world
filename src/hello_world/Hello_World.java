d2ecf9804b635d64cd08a3491331f9e3530ef46c
mlin11 <mlin8@uncc.edu>
2017-08-23 13:45:23 -0400

    test1

--- src/hello_world/Hello_World.java
+++ src/hello_world/Hello_World.java
@@ -5,7 +5,6 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Another line");
 	}
 
 }
