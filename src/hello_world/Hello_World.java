2a53c4e163c868f9c68a95e0c0d6d9330175c227
mlin11 <mlin8@uncc.edu>
2017-08-23 13:47:14 -0400

    second commit

--- src/hello_world/Hello_World.java
+++ src/hello_world/Hello_World.java
@@ -5,6 +5,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello world one more time");
 	}
 
 }
